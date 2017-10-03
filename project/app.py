f69af856b5ebd920c7116789db0fea71b964b49f
Kelli Parker <iamkelliparker@gmail.com>
2017-10-03 12:29:39 -0400

    edited index, added markdown file for bios

--- project/app.py
+++ project/app.py
@@ -6,6 +6,8 @@ from flask import Flask
 from flask import Markup
 from flask import g, render_template, url_for, redirect, abort, request
 from datetime import date, datetime
+import misaka as m
+
 
 
 app = Flask(__name__)
@@ -14,14 +16,14 @@ app.debug = True
 page = {
     'title': 'Las Vegas Shooting Victims',
     'title_twitter': 'The victims of the shooting in Las Vegas'.decode('utf-8'),
-    'url': 'http://interactive.nydailynews.com/project/vegas-victims/',
+    'url': 'http://interactive.nydailynews.com/project/las-vegas-victims/',
     'description': 'DESCRIPTION',
     'author': '"NAME", "Interactive Project"',
     'datestamp': '2017-10-03',
     'keywords': 'Las Vegas, mass shooting, gun, violence',
     'keywords_array': '"las vegas","mass shooting","gun","violence"',
-    'shareimg': 'http://interactive.nydailynews.com/project/vegas-victims/static/img/vegas-victims.jpg',
-    'shareimg_static': 'http://interactive.nydailynews.com/project/mta-funding/static/img/vegas-victims.jpg',
+    'shareimg': 'http://interactive.nydailynews.com/project/vegas-victims/static/img/las-vegas-victims.jpg',
+    'shareimg_static': 'http://interactive.nydailynews.com/project/mta-funding/static/img/las-vegas-victims.jpg',
     'shareimgdesc': '',
 }
 
@@ -32,9 +34,48 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    content = { 'intro': '', 'sections': [] }
+    fh = open('bios.md', 'rb')
+    story = fh.read().split('^^^^^^')
+    fh.close()
+
+    content['intro'] = m.html(story[0])
+    i = 0
+    for s, section in enumerate(story[1:]):
+        # This fixes some formatting issues with variable numbers of newlines in bios.md
+        section = section.strip("\n")
+        items = []
+        parts = section.split("\n\n")
+        for item in parts:
+            mkup = m.html(item)
+            mkup = mkup.replace('</h4>', '</h4>\n<div id="read-more-%d" class="read-more collapsed section-%d" onclick="clicker(%d);">' % (i, s, i))
+            items.append(mkup)
+            i += 1
+
+        markup = '</div>\n</li>\n\n<li>\n'.join(items)
+        
+        # Add the hr's
+        markup = markup.replace('<h3>', '<hr>\n<h3>')
+        # Add the opening ul
+        # We add an expand-all link below the first header
+        if s == 0:
+            markup = markup.replace('</h2>', '</h2>\n<div class="expand-all" onclick="expand_all();"><a>expand all</a></div>\n<ul><li>')
+        else:
+            markup = markup.replace('</h2>', '</h2>\n<ul><li>')
+        # Add the closing ul
+        content['sections'].append('%s\n</div>\n</li>\n</ul>' % markup)
+
+    # Get the most-recent headline
+    fh = open('tag-russia-1.html', 'rb')
+    latest = fh.read()
+    fh.close()
+
     response = {
         'app': app,
+        'latest': latest.decode('utf-8'),
+        'content': content
     }
+
     return render_template('index.html', response=response)
 
 @app.template_filter(name='last_update')
